f788be12ec7af8b62745a2832ef429cc48c36bf6
Sandesh <sandesh0799@gmail.com>
2024-08-15 22:02:01 +0530

    production url

--- client/src/redux/constants/BASE_URL.js
+++ client/src/redux/constants/BASE_URL.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:9000"
+// export const BASE_URL = "http://localhost:9000"
+export const BASE_URL = "https://e-commarce-stbx-46kpvlmh4-sandeshs-projects-30f78623.vercel.app"
